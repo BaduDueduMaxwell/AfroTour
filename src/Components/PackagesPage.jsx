1341dde8bfbd34ff4d350a106dc7ebc5697c4d25
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-08-08 23:24:18 +0000

    Router react

---
+++ src/Components/PackagesPage.jsx
@@ -0,0 +1,257 @@
+export default function Packages() {
+  return (
+    <div className="m-16">
+      <h1 className="text-3xl font-extrabold ml-6 m-2 ">
+        Explore Amazing Places like:
+      </h1>
+
+      {/* Card 1 */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1549643276-fdf2fab574f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c3BhaW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+              alt="Spain"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Màlaga, Spain
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 2 */}
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1571406252241-db0280bd36cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNhbnRvcmluaXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Santorini"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Santorini, Greece
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 3 */}
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1552751118-d3cde54807de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHZlbmljZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Venice"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Venice, Italy
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 4 */}
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+        {/* Card 5 */}
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 6 */}
+
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 7 */}
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+
+        {/* Card 8 */}
+        <div className="rounded  m-5 overflow-hidden shadow-lg flex flex-col">
+          <a href="#">
+            <img
+              className="w-full"
+              src="https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=1000&q=60"
+              alt="Paris"
+            />
+          </a>
+
+          <div className="px-6 py-4 mb-auto">
+            <a
+              href="#"
+              className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
+            >
+              Eiffel, France
+            </a>
+            <p className="text-gray-500 text-sm">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.
+            </p>
+          </div>
+          <div className="flex flex-row px-6 py-2 text-xs">
+            <span className=" text-blue-400 text-xs">$</span>1400{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
