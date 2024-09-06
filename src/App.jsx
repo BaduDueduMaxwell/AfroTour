8c3051f9e2366c3a3fc8a9a720a5a34b6697ffb0
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-06 19:01:10 +0000

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,20 @@
-// import Sidenav from "./Components/Sidenav";
-// import Homepage from "./Components/Homepage";
-// import Footer from "./Components/Footer";
-// import Packages from "./Components/PackagesPage";
-
-// // function App() {
-//     switch (window.location.pathname){
-//         case "/":
-//             break;
-//             case "/Homepage"
-//     }
-//   return
-// }
-
-// export default App;
-
-// import {
-//   createBrowserRouter,
-//   Routes,
-//   Route,
-//   Link,
-//   NavLink,
-//   createRoutesFromElements,
-//   Router,
-// } from "react-router-dom";
-
-// pages
-import Sidenav from "./Components/Sidenav";
 import Homepage from "./Components/Homepage";
-import Footer from "./Components/Footer";
+import Footer from "./Components/Footer/Footer";
 import Packages from "./Components/PackagesPage";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-    <Sidenav className="margin-nav"/>
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="Packages" element={<Packages />} />
-    </Routes>
-    <Footer/>
+      <Navbar className="margin-nav" />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="Packages" element={<Packages />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
-
-// function App(){
-//   return (
-//     <>
-//     <Sidenav/>
-//     <Homepage/>
-//     <Footer/>
-//     <Packages/>
-//     </>
-//   )
-// }
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route>
-//       <Route index element={<Sidenav />} />
-//       <Route path="homepage" element={<Homepage />} />
-//       <Route path="footer" element={<Footer />} />
-//       <Route path="packages" element={<Packages />} />
-//     </Route>
-//   )
-// );
-
-// function App() {
-//   return <RouterProvider router={router} />;
-// }
 
 export default App;
