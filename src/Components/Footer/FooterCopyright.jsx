2ea332bd61131f9d6bf15217355d527657a713f4
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-17 22:56:16 +0000

    fix:corrected color for footer

--- src/Components/Footer/FooterCopyright.jsx
+++ src/Components/Footer/FooterCopyright.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function FooterCopyright() {
   return (
-    <span className="bg-gray-800 text-white py-6">
+    <span className="bg-gray-900 text-white py-6">
       © 2023 <a href="https://flowbite.com/">AfroTour™</a>. All Rights Reserved.
     </span>
   );
