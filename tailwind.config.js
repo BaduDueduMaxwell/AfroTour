6f0e4d01dc3cdb65bdba44066c1185c0502ed53e
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-17 22:18:57 +0000

    update:reinstalled material tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = withMT({
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         sky: {
           400: "#38bdf8",
+          500: "#0ea5e9",
           600: "#0284c7",
+          700: "#0369a1",
+          800: "#075985",
+          900: "#0c4a6e",
         },
-        teal: {
-          200: "#99f6e4",
+        gray: {
+          200: "#e5e7eb", // Border color in dark mode
+          600: "#4b5563", // Dark border color in dark mode
+          800: "#1f2937",
+          900: "#111827", // Dark mode background color
         },
         slate: {
           50: "#f8fafc",
-        },
-        gray: {
-          400: "#9ca3af",
+          950: "#020617" // Example light slate color
         },
       },
     },
   },
   plugins: [],
-};
+});
