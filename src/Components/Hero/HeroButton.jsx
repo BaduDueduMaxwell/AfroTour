6457e9253ee79f9335251107e9aaf56d97ab0266
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-17 22:38:31 +0000

    feat:routing btn to trip-details page

--- src/Components/Hero/HeroButton.jsx
+++ src/Components/Hero/HeroButton.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function HeroButton({ text }) {
   return (
-    <button className="bg-sky-700 text-slate-50 text-lg sm:text-2xl font-medium px-3 py-2 sm:px-4 sm:py-2 rounded shadow">
-      {text}
-    </button>
+    <Link to="/trip-details">
+      <button className="bg-sky-700 text-slate-50 text-lg sm:text-2xl font-medium px-3 py-2 sm:px-4 sm:py-2 rounded shadow">
+        {text}
+      </button>
+    </Link>
   );
 }
