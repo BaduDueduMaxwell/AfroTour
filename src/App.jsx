3fc6ae6f8100555b8c15f3a16602c50585aa0545
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-17 22:34:45 +0000

    update:routing of prev & new pages & components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import Homepage from "./Components/Homepage";
-import Footer from "./Components/Footer/Footer";
-import Packages from "./Components/PackagesPage";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import PackagesPage from "./Pages/PackagesPage";
+import TripDetailsPage from "./Pages/TripDetailsPage";
+import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
+import AboutPage from "./Pages/AboutPage";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
@@ -10,7 +14,10 @@ function App() {
       <Navbar className="margin-nav" />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="Packages" element={<Packages />} />
+        <Route path="packages" element={<PackagesPage />} />
+        <Route path="/trip-details" element={<TripDetailsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
