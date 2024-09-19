f6485c60a33c72853b921d37a86c0b6827c29d06
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-09-19 21:22:39 +0000

    update:commented out logo

--- src/Components/Navbar/Logo.jsx
+++ src/Components/Navbar/Logo.jsx
@@ -4,11 +4,11 @@ export default function Logo() {
   return (
     <div>
       <a href="/" className="flex items-center">
-        <img
+        {/* <img
           src="src/assets/afrotour.webp"
           className="h-8 mr-3"
           alt="AfroTour Logo"
-        />
+        /> */}
         <span className="self-center text-2xl font-semibold whitespace-nowrap text-slate-50">
           AfroTour
         </span>
